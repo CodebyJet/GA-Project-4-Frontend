8a94bfc5baccabf5eb1efc4ea2127b4d7c473130
Jet <thazledine@hotmail.co.uk>
2023-01-27 12:03:48 +0000

    more slashes

--- src/lib/api.js
+++ src/lib/api.js
@@ -3,13 +3,13 @@ import { AUTH } from './auth';
 
 const ENDPOINTS = {
   allQuizes: `${process.env.REACT_APP_BASE_URL}/api/quiz/`,
-  singleQuiz: (id) => `${process.env.REACT_APP_BASE_URL}/api/quiz/${id}`,
+  singleQuiz: (id) => `${process.env.REACT_APP_BASE_URL}/api/quiz/${id}/`,
   allMessages: `${process.env.REACT_APP_BASE_URL}/api/mail/`,
-  singleMessage: (id) => `${process.env.REACT_APP_BASE_URL}/api/mail/${id}`,
+  singleMessage: (id) => `${process.env.REACT_APP_BASE_URL}/api/mail/${id}/`,
   allUsers: `${process.env.REACT_APP_BASE_URL}/api/auth/user/`,
-  singleUser: (id) => `${process.env.REACT_APP_BASE_URL}/api/auth/user/${id}`,
+  singleUser: (id) => `${process.env.REACT_APP_BASE_URL}/api/auth/user/${id}/`,
   allDescriptions: `${process.env.REACT_APP_BASE_URL}/api/description/`,
-  singleDescription: (id) => `${process.env.REACT_APP_BASE_URL}/api/description/${id}`,
+  singleDescription: (id) => `${process.env.REACT_APP_BASE_URL}/api/description/${id}/`,
   login: `${process.env.REACT_APP_BASE_URL}/api/auth/login/`,
   register: `${process.env.REACT_APP_BASE_URL}/api/auth/register/`,
   cloudinary: `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUDINARY_CLOUD_NAME}/image/upload`
